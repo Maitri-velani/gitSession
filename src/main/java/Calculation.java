b6fd8ddaf9794e1c7290a4f1cddf6ab969a8c3ac
maitri <maitrivelani999@gmail.com>
2018-01-04 18:55:53 +0530

    Calculation class is added

---
+++ src/main/java/Calculation.java
@@ -0,0 +1,31 @@
+public class Calculation {
+    private int number1,number2;
+
+    public int getNumber1() {
+        return number1;
+    }
+
+    public void setNumber1(int number1) {
+        this.number1 = number1;
+    }
+
+    public int getNumber2() {
+        return number2;
+    }
+
+    public void setNumber2(int number2) {
+        this.number2 = number2;
+    }
+
+    public void add(int number1,int number2)
+    {
+        int add=number1+number2;
+        System.out.println("Addition is: "+add);
+    }
+
+    public static void main(String[] args) {
+        Calculation calc = new Calculation();
+        calc.add(20,10);
+
+    }
+}
