176b7662479ccb51c22bc925084cce06fc2fdd31
mahima <mahima.bais@brevitaz.com>
2018-01-05 11:11:19 +0530

    Call message class method

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,7 +3,8 @@ public class Hello {
     public void diplayArithmatic(int ans){
 
         System.out.println("Ans is: "+ans);
-
+        Message message = new Message();
+        message.displayMessage();
     }
 
 }
