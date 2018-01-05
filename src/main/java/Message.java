e3480b1926260187f990b4ed5c94e0a675968465
mahima <mahima.bais@brevitaz.com>
2018-01-05 10:57:28 +0530

    Not to commit

--- src/main/java/Message.java
+++ src/main/java/Message.java
@@ -2,7 +2,8 @@ public class Message {
 
     void displayMessage(){
 
-        System.out.println("Hello i'm here.");
+        System.out.println("Hello i'm mahima.");
+        System.out.println("Hello i'm mahima.");
     }
 
 }
